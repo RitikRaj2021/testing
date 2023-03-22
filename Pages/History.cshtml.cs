0fc98bd21538f07b588fd3125fd04a8881bf8c21
RitikRaj2021 <88115183+RitikRaj2021@users.noreply.github.com>
2023-03-22 19:12:51 +1300

    first commit to main

---
+++ Pages/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NETCoreWebAppTest.Pages
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
